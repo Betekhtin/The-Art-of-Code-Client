872aba47f3ae48a60008292dbc68cc9e3b8ff7ae
Betekhtin <st040186@student.spbu.ru>
2017-11-18 16:12:06 +0300

    Init + Tile generation

---
+++ The Art of Code Client/Assets/Scripts/LocationController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using LitJson;
+
+public class LocationController : MonoBehaviour {
+
+    private JsonData locationData;
+    static private float _tile_size_ = 2.5f;
+    static private string _sprite_path_ = "Sprites/";
+    private Tile[,] location;
+
+    public GameObject tilePrefab;
+    
+    // Load location
+    void Start () {
+
+        // Get location json
+        locationData = JsonMapper.ToObject(File.ReadAllText(Application.dataPath + "/Resources/test_location.json"));
+        
+        // Generate tiles
+        int size_x = (int)locationData["matrix"]["size_x"];
+        int size_y = (int)locationData["matrix"]["size_y"];
+        location = new Tile[size_x, size_y];
+   
+        for (int x = 0; x < size_x; ++x) {
+            for (int y = 0; y < size_y; ++y) {
+                GameObject tile = Instantiate(tilePrefab);
+                tile.transform.SetParent(transform, false);
+                tile.transform.position = new Vector3(x * _tile_size_, y * _tile_size_, 0);
+                Debug.Log(_sprite_path_ + (string)locationData["matrix"]["textures"][x][y]);
+                tile.GetComponent<SpriteRenderer>().sprite = 
+                    Resources.Load(_sprite_path_ + (string)locationData["matrix"]["textures"][x][y], typeof(Sprite)) as Sprite;
+                location[x, y] = new Tile(tile);
+            }
+        }
+    }
+
+}
---
+++ The Art of Code Client/Assets/Scripts/Tile.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile{
+
+    private GameObject tile, obj;
+    
+    public Tile(GameObject tile, GameObject obj = null) {
+        this.tile = tile;
+        this.obj = obj;
+    }
+    
+    public void setObject(GameObject obj) {
+        this.obj = obj;
+    }
+
+    public void setSprite(Sprite sprite) {
+        tile.GetComponent<SpriteRenderer>().sprite = sprite;
+    }
+
+}
