ea9541227322b6b3cc8882cbee0681b1fbbc8185
Betekhtin <st040186@student.spbu.ru>
2017-11-19 05:24:23 +0300

    Objects rendering

--- The Art of Code Client/Assets/Scripts/LocationController.cs
+++ The Art of Code Client/Assets/Scripts/LocationController.cs
@@ -11,10 +11,13 @@ public class LocationController : MonoBehaviour {
     static private float _tile_size_ = 1f; //wtf
     static private string _sprite_path_ = "Sprites/";
     private Tile[,] location;
-    private Tile[] objects;
+    private JsonData objectsInfo;
     private Socket socket;
+    public GameObject player;
+    private JsonData heroInfo;
     private bool dataRecieved = false;
-
+    private bool moved = false;
+    private int x, y;
     public GameObject tilePrefab;
     //public GameObject testObject;
     
@@ -28,6 +31,7 @@ public class LocationController : MonoBehaviour {
         socket.On("initData", (json) =>
         {
             Debug.Log("Got initial data.");
+            Debug.Log(json);
 
             // Get location json
             locationData = JsonMapper.ToObject((string)json);
@@ -37,13 +41,29 @@ public class LocationController : MonoBehaviour {
 
         socket.On("move", (json) =>
         {
+            Debug.Log("Moved.");
+            Debug.Log(json);
+
             JsonData moveData = JsonMapper.ToObject((string)json);
 
             int new_position_x = (int)moveData["newPosition"]["x"];
             int new_position_y = (int)moveData["newPosition"]["y"];
 
+            if (heroInfo["id"] == moveData["object"]["id"])
+            {
+                moved = true;
+                x = new_position_x;
+                y = new_position_y;
+            }
             //location
-
+            /*
+            int hero_pos_x = (int)heroInfo["positionX"];
+            int hero_pos_y = (int)heroInfo["positionY"];
+            location[hero_pos_x, hero_pos_y].setObject(null);
+            location[new_position_x, new_position_y].setObject(player);
+            heroInfo["positionX"] = new_position_x;
+            heroInfo["positionY"] = new_position_y;
+            */
         });
 
     }
@@ -54,7 +74,7 @@ public class LocationController : MonoBehaviour {
         if (dataRecieved)
         {
 
-            Debug.Log(locationData.ToString());
+            dataRecieved = false;
 
             // Generate tiles
             int size_x = (int)locationData["locations"]["matrix"]["size_x"];
@@ -74,10 +94,51 @@ public class LocationController : MonoBehaviour {
                 }
             }
 
+            /*
+   id int unsigned not null auto_increment,
+   login varchar(60) not null,
+   userId int unsigned not null,
+   positionX int unsigned not null,
+   positionY int unsigned not null,
+   location varchar(60) not null,
+   HP decimal(5,2) not null,
+   maxHP decimal(6,2) not null,
+   attack int unsigned not null,
+   defence int unsigned not null,
+   exp int unsigned not null,
+   lvl int unsigned not null,
+   primary key (id) 
+             */
+            
+            heroInfo = locationData["hero"];
+            int hero_pos_x = (int)heroInfo["positionX"];
+            int hero_pos_y = (int)heroInfo["positionY"];
+            location[hero_pos_x, hero_pos_y].setObject(player, (int)heroInfo["id"]);
 
+            /*
+  [{"id":1,"positionX":4,"positionY":6,"type":"static","name":"tree"},{"id":2,"positionX":12,"positionY":8,"type":"static","name":"tree"},{"id":3,"positionX":20,"positionY":17,"type":"static","name":"tree"},{"id":4,"positionX":1,"positionY":7,"type":"static","name":"tree"},{"id":5,"positionX":29,"positionY":14,"type":"static","name":"tree"},{"id":6,"positionX":17,"positionY":29,"type":"static","name":"tree"},{"id":7,"positionX":78,"positionY":82,"type":"static","name":"tree"},{"id":8,"positionX":22,"positionY":1,"type":"static","name":"tree"},{"id":9,"positionX":7,"positionY":19,"type":"static","name":"tree"}]
+             */
+            objectsInfo = locationData["objects"];
+            int objects_count = (int)locationData["objectsCount"];
+            for (int i = 0; i < objects_count; ++i)
+            {
+                JsonData current_elem = locationData["objects"][i];
+                int pos_x = (int)current_elem["positionX"];
+                int pos_y = (int)current_elem["positionY"];
+                GameObject newObject = new GameObject();
+                newObject.AddComponent<SpriteRenderer>();
+                newObject.GetComponent<SpriteRenderer>().sprite =
+                    Resources.Load(_sprite_path_ + (string)current_elem["name"], typeof(Sprite)) as Sprite;
+                location[pos_x, pos_y].setObject(newObject, (int)current_elem["id"]);
+            }
 
-            dataRecieved = false;
         }
+        if (moved)
+        {
+            moved = false;
+            player.transform.position = new Vector2(x, y);
+        }
+
     }
 
 }
--- The Art of Code Client/Assets/Scripts/Tile.cs
+++ The Art of Code Client/Assets/Scripts/Tile.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class Tile{
 
     private GameObject tile, obj;
-    
-    public Tile(GameObject tile, GameObject obj = null) {
+    private int id;
+
+    public Tile(GameObject tile, int id = -1, GameObject obj = null) {
         this.tile = tile;
         this.obj = obj;
     }
     
-    public void setObject(GameObject obj) {
+    public void setObject(GameObject obj, int id) {
+        this.id = id;
         this.obj = obj;
         this.obj.transform.SetParent(tile.transform, false);
         this.obj.transform.localPosition = new Vector3(0, 0, 0);
